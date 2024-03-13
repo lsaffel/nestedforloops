e235638f738ba69214e2a93b93aadcb16629bc26
Lisa <llvvss@icloud.com>
2024-03-13 13:16:04 -0600

    Readme modified, then built again and tested. It works

--- src/nestedforloops.cpp
+++ src/nestedforloops.cpp
@@ -4,7 +4,14 @@ using namespace std;
 int main()
 {
     // Multiplication table
-    cout << "Hi there!!" << endl;
-    
+
+    for (int i = 1; i <= 10; i++){
+        for (int j = 1; j <= 10; j++) {
+            cout << i << " * " << j << " = " << i * j << endl;
+        }
+        cout << endl;
+    }    
+
+
     return 0;
 }
